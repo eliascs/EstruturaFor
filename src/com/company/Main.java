dbc4dcb23d82699f645b48ba984f8d6f27c565c4
Elias Cardoso <eliascardosodasilva@hotmail.com>
2022-03-08 12:27:18 -0300

    EstruturaFor

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //contador
+
+        for (int numero = 1; numero <= 10; numero++) {
+            System.out.println("O numero atual é : " + numero);
+
+            // for (inicialização; condição ; atualização);
+        }
+    }
+}
